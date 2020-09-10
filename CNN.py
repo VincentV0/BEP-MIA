fe42e07ba28a44086125d72d7f5c36eac3f8fc65
VincentV0 <v.r.j.vousten@student.tue.nl>
2020-09-10 20:27:53 +0200

    multiple changes

--- CNN.py
+++ CNN.py
@@ -3,10 +3,8 @@
 from __future__ import print_function
 
 import os
-from params import * # Load the parameters set in a different
-import tensorflow as tf
-tf.get_logger().setLevel('WARNING')
-tf.autograph.set_verbosity(2)
+import sys
+from importlib import import_module
 from skimage.transform import resize
 from skimage.io import imsave
 import numpy as np
@@ -17,30 +15,78 @@ from tensorflow.keras.layers import Input, Activation, concatenate, Conv2D, MaxP
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras import backend as K
-from data import load_data, write_save_data
+from data import load_data
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import f1_score, accuracy_score, auc, roc_curve, roc_auc_score, recall_score, precision_score, matthews_corrcoef, confusion_matrix, average_precision_score
 import scipy.io as sio
 import matplotlib.pyplot as plt
 from datetime import datetime
 import xlsxwriter
+import matplotlib.pyplot as plt
+
 
 K.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
-def time_diff_format(start,end):
+################### Simple functions ###########################################
+def time_diff_format(start, end):
+    """
+    DESCRIPTION: This function calculates the time difference and returns it
+    in a mm:ss format
+
+    Parameters:
+    ----------
+    start: TYPE datetime
+        the start-time
+    end: TYPE datetime
+        the end-time
+
+    Returns
+    -------
+    time_formatted : TYPE string
+        The time difference in mm:ss format
+
+    """
     time_taken = (end - start).seconds;
     time_mins = time_taken // 60;
     time_secs = time_taken - (time_mins * 60);
     time_formatted = '{}:{}'.format(time_mins,time_secs)
     return time_formatted
 
+################################################################################
+def plot_ROC_curve(fpr, tpr, folder, filetime):
+    """
+    DESCRIPTION: This function plots the ROC curves of the different runs
+    and saves it to a png file.
 
+    Parameters:
+    ----------
+    fpr: the list with the false positive rates for every run
+    tpr: the list with the true positive rates for every run
+    folder: the folder to save to
+    filetime: the time to be included in the filename.
+
+    Returns
+    -------
+    time_formatted : TYPE string
+        The time difference in mm:ss format
+
+    """
+    fig,ax = plt.subplots()
+    for i in range(pm.runNum):
+        ax.plot(fpr[i], tpr[i], marker = '.', label='Run {}'.format(i),alpha=0.3)
+    ax.set_xlabel('False positive rate')
+    ax.set_ylabel('True positive rate')
+    ax.legend()
+    fig.savefig(folder + "ROC_curve " + filetime)
+
+################################################################################
 def make_labels(targets):
     """
     DESCRIPTION
     -----------
     This function creates the labels for images based on the existance of BP:
     if the sum of values in the target is zeros returns '0' else returns '1'
+
     Parameters
     ----------
     targets : TYPE np.ndarray
@@ -57,11 +103,10 @@ def make_labels(targets):
     targetsReshaped = targets.reshape(targetShape[0],targetShape[1]*targetShape[2]);
     binary_targets = np.max(targetsReshaped,axis=1);
     binary_targets = binary_targets / 255;
-
     return binary_targets
 
 
-###########################################
+################################################################################
 def split_data(imgs, targets, nb_folds = 5):
     """
     DESCRIPTION:
@@ -103,7 +148,7 @@ def split_data(imgs, targets, nb_folds = 5):
     return X_train,X_test,y_train,y_test
 
 
-#############################################################################################################
+################################################################################
 def normalization(train_data, train_labels, test_data, test_labels):
     """
     DESCRIPTION:
@@ -148,7 +193,6 @@ def normalization(train_data, train_labels, test_data, test_labels):
 
     return train_data, train_labels, test_data, test_labels
 
-
 ########################################################
 def preprocess(imgs):
     """
@@ -167,13 +211,65 @@ def preprocess(imgs):
         Array with all the images, after the preprocessing step.
 
     """
-    imgs_p = np.ndarray((imgs.shape[0], img_rows_ds, img_cols_ds), dtype=np.uint8)
-    imgs_p = resize(imgs[i], (imgs.shape[0], img_rows_ds, img_cols_ds), preserve_range=True)
+    imgs_p = np.ndarray((imgs.shape[0], pm.img_rows_ds, pm.img_cols_ds), dtype=np.uint8)
+    imgs_p = resize(imgs, (imgs.shape[0], pm.img_rows_ds, pm.img_cols_ds), preserve_range=True)
     imgs_p = imgs_p[..., np.newaxis]
     return imgs_p
 
-
 ################################################################################
+def write_save_data(hist):
+    """
+    DESCRIPTION:
+    -----------
+    Write the accuracies and losses after every epoch to an xlsx-file, and the
+    evaluation scores on the test set to a seperate mat file.
+
+    Parameters
+    ----------
+    hist : TYPE list
+        Constains the validation/training losses and accuracies for every epoch.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Get the datetime for a overall filename
+    filename_time = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+    folder = pm.save_path + 'RUN at ' + filename_time + '/';
+    os.mkdir(folder)
+
+    plot_ROC_curve(pm.fpr_list,pm.tpr_list, folder, filename_time)
+
+    # Save the accuracy scores to a .mat file
+    sio.savemat(folder + 'CNN_Results_' + filename_time + '.mat', \
+        {'precisionNet': pm.precisionNet,'AUCNet':pm.AUCNet,
+        'recallNet': pm.recallNet, 'f1Net': pm.f1Net,'accNet': pm.accNet,
+        'fpr':pm.fpr_list, 'tpr':pm.tpr_list,
+        'time': pm.time_list})
+
+    # Save the loss/accuracy history to a xlsx file
+    workbook = xlsxwriter.Workbook(folder + 'history ' + filename_time + '.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.write('B1', 'epoch')
+    worksheet.write('C1', 'val_loss')
+    worksheet.write('D1', 'val_acc')
+    worksheet.write('E1', 'train_loss')
+    worksheet.write('F1', 'train_acc')
+
+    for runnr in range(1,pm.runNum+1):
+        for i in range(pm.nb_epochs):
+            line = (runnr-1)*pm.nb_epochs + i + 1 + runnr;
+            worksheet.write(line, 0, 'RUN {}'.format(runnr))
+            worksheet.write(line, 1, i+1)
+            worksheet.write(line, 2, hist[runnr-1]['val_loss'][i])
+            worksheet.write(line, 3, hist[runnr-1]['val_accuracy'][i])
+            worksheet.write(line, 4, hist[runnr-1]['loss'][i])
+            worksheet.write(line, 5, hist[runnr-1]['accuracy'][i])
+
+    workbook.close()
+
+############## Main function ###################################################
 def train_and_predict():
     """
     DESCRIPTION:
@@ -201,7 +297,7 @@ def train_and_predict():
     labels = make_labels(labels)
     data = preprocess(data)
 
-    X_train, X_test, y_train, y_test = split_data(data, labels, nb_folds)
+    X_train, X_test, y_train, y_test = split_data(data, labels, pm.nb_folds)
 
     print('-'*30)
     print('normalize data...')
@@ -212,7 +308,7 @@ def train_and_predict():
     print('Make labels categorical...')
     print('-'*30)
 
-    trainingLabels = keras.utils.to_categorical(trainingLabels, NB_CLASSES)
+    trainingLabels = keras.utils.to_categorical(trainingLabels, pm.NB_CLASSES)
 
     print('-'*30)
     print('Creating and compiling model...')
@@ -220,7 +316,7 @@ def train_and_predict():
 
 
     model = keras.Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape = input_shape_ds))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape = pm.input_shape_ds))
     model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -240,12 +336,12 @@ def train_and_predict():
     model.add(BatchNormalization())
     model.add(Dense(8, kernel_initializer='glorot_uniform', activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(NB_CLASSES))
+    model.add(Dense(pm.NB_CLASSES))
     model.add(Activation('softmax'))
 
-    model.compile(loss=loss_function, optimizer=model_optimizer, metrics=model_metrics)
-    history = model.fit(trainingFeatures, trainingLabels, batch_size=train_batch_size, epochs=nb_epochs, \
-        verbose=verbose_mode, shuffle=True, validation_split=validation_fraction) #class_weight = class_w
+    model.compile(loss=pm.loss_function, optimizer=pm.model_optimizer, metrics=pm.model_metrics)
+    history = model.fit(trainingFeatures, trainingLabels, batch_size=pm.train_batch_size, epochs=pm.nb_epochs, \
+        verbose=pm.verbose_mode, shuffle=True, validation_split=pm.validation_fraction) #class_weight = class_w
 
     predicted_testLabels = model.predict_classes(testFeatures,verbose = 0)
     soft_targets_test = model.predict(testFeatures,verbose = 0)
@@ -256,23 +352,41 @@ def train_and_predict():
     print('-'*30)
 
     # Calculate scores and add to their list
-    precisionNet.append(precision_score(testLabels, predicted_testLabels))
-    recallNet.append(recall_score(testLabels, predicted_testLabels))
-    accNet.append(accuracy_score(testLabels, predicted_testLabels))
-    f1Net.append(f1_score(testLabels, predicted_testLabels))
-    AUCNet.append(roc_auc_score(testLabels, soft_targets_test[:,1]))
+    pm.precisionNet.append(precision_score(testLabels, predicted_testLabels))
+    pm.recallNet.append(recall_score(testLabels, predicted_testLabels))
+    pm.accNet.append(accuracy_score(testLabels, predicted_testLabels))
+    pm.f1Net.append(f1_score(testLabels, predicted_testLabels))
+    pm.AUCNet.append(roc_auc_score(testLabels, soft_targets_test[:,1]))
     fpr, tpr, _ = roc_curve(testLabels, soft_targets_test[:, 1])
-    fpr_list.append(fpr)
-    tpr_list.append(tpr)
+
+    pm.fpr_list.append(fpr)
+    pm.tpr_list.append(tpr)
 
     end_time = datetime.now()
     total_time = time_diff_format(start_time, end_time)
-    time_list.append(total_time)
+    pm.time_list.append(total_time)
 
-    history_list.append(history.history)
+    pm.history_list.append(history.history)
 
 ################################################################################
 if __name__ == '__main__':
-    for i in range(runNum):
+    # Check whether a custom parameter file has been defined
+    if len(sys.argv) == 1:
+        pm = import_module("params_default")
+    elif len(sys.argv) == 2:
+        # Try to load the custom parameter file, otherwise return an error.
+        try:
+            pm = import_module(sys.argv[1])
+        except:
+            print("Could not find parameter file '{}'.".format(sys.argv[1]))
+            sys.exit(1)
+    else:
+        # When too many arguments are given.
+        print("Only one argument is allowed to define the parameter file.")
+        sys.exit(2)
+
+
+    for run in range(pm.runNum):
         train_and_predict()
-    write_save_data(history_list)
+
+    write_save_data(pm.history_list)
